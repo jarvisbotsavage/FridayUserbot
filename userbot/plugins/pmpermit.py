efd2cf0ae107fb7c602fcb231fa565f09b00dbc1
Harshtech2019 <53529994+Harshtech2019@users.noreply.github.com>
2020-07-10 14:23:02 +0300

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -8,12 +8,6 @@ from telethon import events, errors, functions, types
 from userbot import ALIVE_NAME, CUSTOM_PMPERMIT
 from userbot.utils import admin_cmd
 
-PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
-if PMPERMIT_PIC is None:
-  WARN_PIC = "https://telegra.ph/file/82c595986872349e5ba1a.jpg"
-else:
-  WARN_PIC = PMPERMIT_PIC
-
 PM_WARNS = {}
 PREV_REPLY_MESSAGE = {}
 
